ad8d21a08a0909e32838e04efadec96b2b76a7ff
Daniel Gorelick <dqgorelick@gmail.com>
2016-11-17 14:22:54 -0500

    updates script to move new scne into the folder

--- generateScene.sh
+++ generateScene.sh
@@ -11,7 +11,9 @@ then
     sed "s/emptyScene/$SCENE_NAME/g" contourApp/src/scenes/emptyScene/emptyScene.cpp > "$SCENE_NAME/$SCENE_NAME.cpp"
     rm $SCENE_NAME/emptyScene.cpp
     rm $SCENE_NAME/emptyScene.h
-    echo -e "${CYAN}SUCCESS!${NC}\nCreated folder: $SCENE_NAME\nYou can move the folder into the project now"
+    mv $SCENE_NAME contourApp/src/scenes/
+    open contourApp/src/scenes/$SCENE_NAME
+    echo -e "${CYAN}SUCCESS!${NC}\nCreated folder: $SCENE_NAME\nYou can drag the folder into your project"
 else
     echo -e "${RED}ERROR${NC}\nInstructions: './generate_scene.sh NEW_SCENE_NAME'"
 fi
